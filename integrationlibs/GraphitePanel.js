f0b114fe83d669abd07d0e20dc58af5852423e96
Isaac Finnegan <isaacfinnegan@gmail.com>
2014-01-06 19:20:41 -0800

    updating to latest GraphitePanel extension that supports Graphite tempaltes

--- integrationlibs/GraphitePanel.js
+++ integrationlibs/GraphitePanel.js
@@ -20,9 +20,41 @@ if (typeof Graphite == 'undefined') {
         }
     }
 }
+Ext.define('graphtemplate', {
+    extend: 'Ext.data.Model',
+    fields: ['name'],
+    proxy: {
+        type: 'ajax',
+        url: '/dashboard/find-template/',
+        extraParams: {
+            query: null
+        },
+        reader: {
+            type: 'json',
+            root: 'templates'
+        }
+    }
+});
+Ext.define('template_graph', {
+    extend: 'Ext.data.Model',
+    fields: [{
+        name: 'title',
+    }, {
+        name: 'target',
+    }, {
+        name: 'from',
+    }, {
+        name: 'until',
+    }, {
+        name: 'vtitle',
+    }, {
+        name: 'areaMode',
+    }]
+});
 
 
-Graphite.graphiteWindow = function(n, name_key) {
+
+Graphite.graphiteWindow = function(n, name_key, title) {
     var nodelist;
     var hostnamelist = new Array();
     if (n.length > 0) {
@@ -34,16 +66,17 @@ Graphite.graphiteWindow = function(n, name_key) {
         var hostname = nodelist[i].data.name || nodelist[i].data.host_name || nodelist[i].data[name_key];
         hostnamelist.push(hostname);
     }
-    var g = new Graphite.GraphitePanel({
+    var g = Ext.create('Graphite.GraphitePanel', {
         title: undefined
     });
-    var w = new Ext.Window({
-        title: 'Graphite',
+    var w = Ext.create('Ext.Window', {
+        title: title || 'Graphite',
         height: window.innerHeight - 200,
         width: 800,
         layout: 'fit',
         items: g
     });
+    Graphite.log(w);
     w.show();
     g.load(hostnamelist.join(','));
 
@@ -60,7 +93,7 @@ Ext.define('Graphite.GraphitePanel', {
     layout: 'border',
     currentGraph: {
         metric: Graphite.config.defaultMetric,
-        from: '-12hours',
+        from: '-6hours',
         until: 'now'
     },
     system_fqdn: '',
@@ -83,67 +116,33 @@ Ext.define('Graphite.GraphitePanel', {
     },
     loaded: false,
     title: 'Graphite',
-    setGraph: function() {
-        if (!this.centerRegion) {
-            this.centerRegion = this.down('panel[region=center]');
-        }
-        var graphUrl = Graphite.config.path + "?from=";
-        var targets = this.system_fqdn.split(',');
-        graphUrl += this.currentGraph.from + "&until=" + this.currentGraph.until;
-        graphUrl += "&width=" + this.centerRegion.getBox().width + "&height=" + this.centerRegion.getBox().height;
-        for (var l = 0; l < targets.length; l++) {
-            var fqdn = targets[l].split('.');
-            graphUrl += "&target=" + Graphite.config.metricsPrefix + "." + fqdn[0] + "*." + this.currentGraph.metric;
-        }
-        graphUrl += "&_uniq=0.9507563426159322&hideLegend=false&title=" + this.currentGraph.metric;
-        this.layout.centerRegion.update("<img src='" + graphUrl + "'>");
-        this.loaded = true;
-    },
-    load: function(system_fqdn) {
-        this.loaded = false;
-        if (system_fqdn) {
-            this.system_fqdn = system_fqdn;
-        }
-        if (!this.loaded && this.isVisible()) {
-            this.setGraph();
-        }
-        if (!this.westRegion) {
-            this.westRegion = this.down('panel[region=west]');
-        }
-        var targets = this.system_fqdn.split(',');
-        var fqdn = targets[0].split('.');
-
-        this.westRegion.setRootNode({
-            id: Graphite.config.metricsPrefix + '.' + fqdn[0] + '*',
-            text: system_fqdn
-        });
-    },
-    items: [{
-        region: 'west',
-        xtype: 'treepanel',
-        title: 'Metrics',
-        width: 150,
-        collapsed: true,
-        animCollapse: false,
-        collapsible: true,
-        hideCollapseTool: true,
-        titleCollapse: true,
-        preventHeader: true,
-        hideHeaders: true,
-        rootVisible: false,
-        split: false,
-        tbar: [{
-            text: 'Time -12hours',
+    constructor: function(config) {
+        this.items = [{
+            xtype: 'panel',
+            region: 'center',
+            layout: 'fit',
+            html: "This is where the graph will be"
+        }];
+        this.timeSelector_tbar = [{
+            text: 'Time -6hours',
             menu: {
                 xtype: 'menu',
                 listeners: {
                     'hide': {
                         fn: function(menu) {
-                            menu.up('treepanel').collapse();
+                            menu.up('graphitepanel').optionsPanel.collapse();
                         }
                     }
                 },
                 items: [{
+                    text: '-6hours',
+                    handler: function(i) {
+                        var gpanel = i.up('panel[xtype=graphitepanel]');
+                        gpanel.currentGraph.from = i.text;
+                        i.up('button').setText('Time ' + i.text);
+                        gpanel.setGraph();
+                    }
+                }, {
                     text: '-12hours',
                     handler: function(i) {
                         var gpanel = i.up('panel[xtype=graphitepanel]');
@@ -169,55 +168,211 @@ Ext.define('Graphite.GraphitePanel', {
                     }
                 }]
             }
-        }],
-        listeners: {
-            'itemclick': {
-                fn: function(view, rec) {
-                    console.log(rec);
-                    var gpanel = view.up('panel[xtype=graphitepanel]');
-                    gpanel.currentGraph.metric = rec.id.replace(rec.store.getRootNode().id + '.', '');
-                    rec.getOwnerTree().collapse();
-                    gpanel.setGraph();
+        }]
+        this.metricsBrowserSpec = {
+            xtype: 'treepanel',
+            title: 'Metrics',
+            rootVisible: false,
+            listeners: {
+                'itemclick': {
+                    fn: function(view, rec) {
+                        console.log(rec);
+                        var gpanel = view.up('panel[xtype=graphitepanel]');
+                        gpanel.currentGraph.metric = rec.id.replace(rec.store.getRootNode().id + '.', '');
+                        gpanel.optionsPanel.collapse();
+                        gpanel.setGraph();
+                    },
                 },
+                'beforeitemexpand': {
+                    fn: function(node) {
+                        if (!node.isExpandable()) {
+                            return;
+                        }
+                        var node_id = node.id.replace(/^[A-Za-z.\-0-9]+./, '*');
+                        var proxy = node.getOwnerTree().getStore().getProxy();
+                        proxy.setExtraParam('query', (node_id == "") ? "*" : (node_id + ".*"));
+                        proxy.setExtraParam('format', 'treejson');
+                        proxy.setExtraParam('contexts', '1');
+                        proxy.setExtraParam('path', node_id);
+                    },
+                    scope: this
+                }
             },
-            'beforeitemexpand': {
-                fn: function(node) {
-                    if (!node.isExpandable()) {
-                        return;
+            store: Ext.create('Ext.data.TreeStore', {
+                proxy: {
+                    type: 'ajax',
+                    url: Graphite.config.metricsPath + 'find/',
+                    extraParams: {
+                        format: 'treejson',
+                        contexts: 1,
+                    },
+                    pageParam: undefined,
+                    sortParam: undefined,
+                    groupParam: undefined,
+                    filterParam: undefined
+                },
+                root: {
+                    expanded: false,
+                    leaf: true
+                }
+            })
+        };
+        if (Graphite.config.enableTemplates) {
+            this.metricsBrowser = Ext.create('Ext.tree.Panel', this.metricsBrowserSpec);
+            this.templatestore = Ext.create('Ext.data.Store', {
+                model: 'template_graph',
+                proxy: {
+                    type: 'ajax',
+                    url: '/dashboard/load',
+                    extraParams: {
+                        template: Graphite.config.defaultTemplate
+                    },
+                    reader: {
+                        type: 'json',
+                        root: 'state.graphs',
+                        record: 1
                     }
-                    var node_id = node.id.replace(/^[A-Za-z.\-0-9]+./, '*');
-                    var proxy = node.getOwnerTree().getStore().getProxy();
-                    proxy.setExtraParam('query', (node_id == "") ? "*" : (node_id + ".*"));
-                    proxy.setExtraParam('format', 'treejson');
-                    proxy.setExtraParam('contexts', '1');
-                    proxy.setExtraParam('path', node_id);
                 },
-                scope: this
-            }
-        },
-        store: new Ext.data.TreeStore({
-            proxy: {
-                type: 'ajax',
-                url: Graphite.config.metricsPath + 'find/',
-                extraParams: {
-                    format: 'treejson',
-                    contexts: 1,
+                listeners: {
+                    'beforeload': {
+                        fn: function(store, op, eOpts) {
+                            store.getProxy().url = '/dashboard/load/' + (op.params ? op.params.template : store.getProxy().extraParams.template);
+                            return true;
+                        }
+                    }
+                },
+                autoLoad: true
+            });
+            this.optionsPanel = Ext.create('Ext.tab.Panel', {
+                region: 'west',
+                title: 'Graph Options',
+                width: 150,
+                collapsed: true,
+                animCollapse: false,
+                collapsible: true,
+                hideCollapseTool: true,
+                titleCollapse: true,
+                preventHeader: true,
+                hideHeaders: true,
+                split: false,
+                tbar: this.timeSelector_tbar,
+                items: [{
+                        xtype: 'panel',
+                        title: 'Templates',
+                        tbar: [{
+                            xtype: 'combo',
+                            store: Ext.create('Ext.data.Store', {
+                                model: 'graphtemplate',
+                                autoLoad: true,
+                            }),
+                            displayField: 'name',
+                            triggerAction: 'all',
+                            value: Graphite.config.defaultTemplate,
+                            selectOnFocus: true,
+                            listeners: {
+                                'change': {
+                                    fn: function(combo, newvalue, oldvalue, eOpts) {
+                                        this.templatestore.load({
+                                            params: {
+                                                template: newvalue
+                                            }
+                                        });
+                                        // combo.up('panel').setTitle(newvalue);
+                                    },
+                                    scope: this
+                                }
+                            }
+                        }],
+                        items: [
+                            Ext.create('Ext.view.View', {
+                                store: this.templatestore,
+                                tpl: [
+                                    '<tpl for=".">',
+                                    '<div class=template>{title}</div>',
+                                    '</tpl>',
+                                    '<div class="x-clear"></div>'
+                                ],
+                                itemSelector: 'div.template',
+                                multiSelect: false,
+                                emptyText: 'no graphs found in template',
+                                listeners: {
+                                    'selectionchange': function(div, node) {
+                                        this.templateGraph = node[0];
+                                        this.optionsPanel.collapse();
+                                        this.setGraph();
+                                    },
+                                    scope: this
+                                }
+                            })
+                        ]
+                    },
+                    this.metricsBrowser
+                ]
+            });
+            this.items.push(this.optionsPanel);
+        } else {
+            Ext.apply(this.metricsBrowserSpec, {
+                region: 'west',
+                width: 150,
+                collapsed: true,
+                animCollapse: false,
+                collapsible: true,
+                hideCollapseTool: true,
+                titleCollapse: true,
+                preventHeader: true,
+                hideHeaders: true,
+                split: false,
+                tbar: this.timeSelector_tbar
+            });
+            this.metricsBrowser = Ext.create('Ext.tree.Panel', this.metricsBrowserSpec);
+            this.items.push(this.metricsBrowser);
+            this.optionsPanel = this.metricsBrowser
+        }
 
-                },
-                pageParam: undefined,
-                sortParam: undefined,
-                groupParam: undefined,
-                filterParam: undefined
-            },
-            root: {
-                expanded: false,
-                leaf: true
+        Graphite.GraphitePanel.superclass.constructor.apply(this, arguments);
+    },
+    setGraph: function() {
+        if (!this.centerRegion) {
+            this.centerRegion = this.down('panel[region=center]');
+        }
+        var graphUrl = Graphite.config.path + "?from=";
+        var targets = this.system_fqdn.split(',');
+        graphUrl += this.currentGraph.from + "&until=" + this.currentGraph.until;
+        graphUrl += "&width=" + this.centerRegion.getBox().width + "&height=" + this.centerRegion.getBox().height;
+        for (var l = 0; l < targets.length; l++) {
+            var fqdn = targets[l].split('.');
+            if (this.templateGraph) {
+                graphUrl += "&areaMode=" + this.templateGraph.get('areaMode');
+                graphUrl += "&vtitle=" + this.templateGraph.get('vtitle');
+                graphUrl += "&title=" + this.templateGraph.get('title');
+
+                var template_targets = this.templateGraph.get('target');
+                for (var tl = 0; tl < template_targets.length; tl++) {
+                    graphUrl += "&target=" + template_targets[tl].replace(/{{id}}/g, fqdn[0] + '*');
+                }
+            } else {
+                graphUrl += "&target=" + Graphite.config.metricsPrefix + "." + fqdn[0] + "*." + this.currentGraph.metric;
+                graphUrl += "&title=" + this.currentGraph.metric;
             }
-        })
-    }, {
-        xtype: 'panel',
-        region: 'center',
-        layout: 'fit',
-        html: "This is where the graph will be"
-    }]
+        }
+        graphUrl += "&_uniq=0.9507563426159322&hideLegend=false";
+        this.layout.centerRegion.update("<img src='" + graphUrl + "'>");
+        this.loaded = true;
+    },
+    load: function(system_fqdn) {
+        this.loaded = false;
+        if (system_fqdn) {
+            this.system_fqdn = system_fqdn;
+        }
+        if (!this.loaded && this.isVisible()) {
+            this.setGraph();
+        }
+        var targets = this.system_fqdn.split(',');
+        var fqdn = targets[0].split('.');
+
+        this.metricsBrowser.setRootNode({
+            id: Graphite.config.metricsPrefix + '.' + fqdn[0] + '*',
+            text: system_fqdn
+        });
+    },
 });
